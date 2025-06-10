431bde6dd9dc9ae64df1c329ff9ab8ce3b13a2cd
Alex <southpawsesto@gmail.com>
2025-06-09 23:05:03 -0700

    Application executable made. Batch file for comiling added to project.

---
+++ AtriaApplication.cmd
@@ -0,0 +1,7 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'AtriaApplication.exe' with Python DLL being found.
+set PATH=c:\users\alex\appdata\local\programs\python\python313;%PATH%
+set PYTHONHOME=C:\Users\Alex\AppData\Local\Programs\Python\Python313
+set NUITKA_PYTHONPATH=C:\Users\Alex\Desktop\Elae;C:\Users\Alex\AppData\Local\Programs\Python\Python313\DLLs;C:\Users\Alex\AppData\Local\Programs\Python\Python313\Lib;C:\Users\Alex\AppData\Local\Programs\Python\Python313;c:\Users\Alex\Desktop\Elae\.venv;c:\Users\Alex\Desktop\Elae\.venv\Lib\site-packages
+"%~dp0AtriaApplication.exe" %*
