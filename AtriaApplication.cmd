07de5b8bb5a1dab867353969cdae3690e26197bc
Alex <southpawsesto@gmail.com>
2025-06-16 16:21:02 -0700

    Additional Testing, added console output

--- AtriaApplication.cmd
+++ AtriaApplication.cmd
@@ -1,7 +1,7 @@
 
 @echo off
 rem This script was created by Nuitka to execute 'AtriaApplication.exe' with Python DLL being found.
-set PATH=c:\users\alex\appdata\local\programs\python\python310;%PATH%
-set PYTHONHOME=C:\Users\Alex\AppData\Local\Programs\Python\Python310
-set NUITKA_PYTHONPATH=C:\Users\Alex\Desktop\Atria;C:\Users\Alex\AppData\Local\Programs\Python\Python310\DLLs;C:\Users\Alex\AppData\Local\Programs\Python\Python310\lib;C:\Users\Alex\AppData\Local\Programs\Python\Python310;C:\Users\Alex\AppData\Local\Programs\Python\Python310\lib\site-packages
+set PATH=c:\users\alex\appdata\local\programs\python\python313;%PATH%
+set PYTHONHOME=C:\Users\Alex\AppData\Local\Programs\Python\Python313
+set NUITKA_PYTHONPATH=C:\Users\Alex\Desktop\Atria;C:\Users\Alex\AppData\Local\Programs\Python\Python313\DLLs;C:\Users\Alex\AppData\Local\Programs\Python\Python313\Lib;C:\Users\Alex\AppData\Local\Programs\Python\Python313;C:\Users\Alex\Desktop\Atria\.venv;C:\Users\Alex\Desktop\Atria\.venv\Lib\site-packages
 "%~dp0AtriaApplication.exe" %*
--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ echo Build Init...
 call .venv\Scripts\activate.bat
 
 nuitka AtriaApplication.py ^
-    --windows-console-mode=disable^
+    --windows-console-mode=force^
     --windows-icon-from-ico=Art\AtriaLogo2.ico^
     --verbose ^
     --show-progress ^
